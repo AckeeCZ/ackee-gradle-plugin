906aac976d7d79c4fde693f4464bdd2a86b7076f
David Bilik <david.bilik@ackee.cz>
2020-10-13 11:11:56 +0200

    ⬆️ Update versions and fix AGP 4.1
    In AGP 4.1.0 visibility of setManifestPlaceholders method
    was changed and it broke our builds. Fix that along with updating
    the versions.

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:3.2.1")
+        classpath("com.android.tools.build:gradle:4.1.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("cz.ackee:build-gradle-plugin:1.0.0-RC13")
     }
