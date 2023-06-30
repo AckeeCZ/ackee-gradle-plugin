3d8fc3bf8069776afa2b5d5c579f9f7e198f6d18
Petr Šíma <petrsima143@gmail.com>
2023-06-30 11:09:55 +0200

    🐛 Fix not applying default proguard file

--- build-gradle-plugin/src/main/kotlin/cz/ackee/gradle/type/CustomBuildTypeCreator.kt
+++ build-gradle-plugin/src/main/kotlin/cz/ackee/gradle/type/CustomBuildTypeCreator.kt
@@ -1,24 +1,29 @@
 package cz.ackee.gradle.type
 
-import com.android.build.api.dsl.ApkSigningConfig
-import com.android.build.api.dsl.ApplicationBuildType
-import org.gradle.api.NamedDomainObjectContainer
+import com.android.build.api.dsl.ApplicationExtension
 
 class CustomBuildTypeCreator(
-    private val buildTypeContainer: NamedDomainObjectContainer<out ApplicationBuildType>,
-    private val signingConfigContainer: NamedDomainObjectContainer<out ApkSigningConfig>
+    private val applicationExtension: ApplicationExtension,
 ) {
+
+    private val buildTypeContainer = applicationExtension.buildTypes
+    private val signingConfigContainer = applicationExtension.signingConfigs
 
     fun maybeCreate(customBuildType: CustomBuildType) {
         buildTypeContainer.maybeCreate(customBuildType.name).apply {
             applicationIdSuffix = customBuildType.appIdSuffix
             customBuildType.appNameSuffix?.let { manifestPlaceholders += mapOf("appNameSuffix" to it) }
-            customBuildType.signingConfigName?.let { signingConfig = signingConfigContainer.getByName(it) }
+            customBuildType.signingConfigName?.let {
+                signingConfig = signingConfigContainer.getByName(it)
+            }
             isMinifyEnabled = customBuildType.isMinifyEnabled
             isShrinkResources = customBuildType.isShrinkResources
 
             if (customBuildType.enableProguard) {
-                proguardFiles("proguard-android-optimize.txt", "proguard-rules.pro")
+                proguardFiles(
+                    applicationExtension.getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
+                )
             }
         }
     }
