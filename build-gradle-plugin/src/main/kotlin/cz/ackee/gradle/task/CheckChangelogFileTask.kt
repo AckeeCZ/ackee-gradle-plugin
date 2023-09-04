989caea9345df974c2b091ce1ce1ac3f42549ab9
Petr Šíma <petr.sima1@ackee.cz>
2023-09-04 11:55:28 +0200

    🐛 Fix CheckChangelogFileTask

--- build-gradle-plugin/src/main/kotlin/cz/ackee/gradle/task/CheckChangelogFileTask.kt
+++ build-gradle-plugin/src/main/kotlin/cz/ackee/gradle/task/CheckChangelogFileTask.kt
@@ -2,6 +2,8 @@ package cz.ackee.gradle.task
 
 import org.gradle.api.DefaultTask
 import org.gradle.api.Project
+import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.tasks.InputDirectory
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.TaskProvider
 import org.gradle.kotlin.dsl.register
@@ -13,9 +15,12 @@ import java.io.File
  */
 abstract class CheckChangelogFileTask : DefaultTask() {
 
+    @get:InputDirectory
+    abstract val changeLogDirectory: DirectoryProperty
+
     @TaskAction
     fun onTaskExecution() {
-        val file = File("${project.rootDir}/outputs/changelog.txt")
+        val file = File(changeLogDirectory.get().asFile, "changelog.txt")
         if (!file.exists()) {
             file.createNewFile()
         }
@@ -27,6 +32,7 @@ abstract class CheckChangelogFileTask : DefaultTask() {
 
         fun registerTask(project: Project): TaskProvider<CheckChangelogFileTask> {
             return project.tasks.register<CheckChangelogFileTask>(createTaskName()) {
+                changeLogDirectory.set(File("${project.rootDir}/outputs"))
                 group = Groups.DEPLOYMENT
             }
         }
