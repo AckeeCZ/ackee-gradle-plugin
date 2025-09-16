8128527f7d325e155f2e00ec064778fd882c5bb9
Jan Mottl <jan.mottl@ackee.cz>
2025-09-16 11:43:58 +0200

    🔥 Remove FetchDetektConfigTask

--- build-gradle-plugin/src/main/kotlin/io/github/ackeecz/gradle/plugin/VerificationsPlugin.kt
+++ build-gradle-plugin/src/main/kotlin/io/github/ackeecz/gradle/plugin/VerificationsPlugin.kt
@@ -2,28 +2,20 @@ package io.github.ackeecz.gradle.plugin
 
 import com.android.build.api.dsl.Lint
 import io.github.ackeecz.gradle.setUpCodeCoverageTasks
-import io.github.ackeecz.gradle.task.FetchDetektConfigTask
 import io.github.ackeecz.gradle.task.copy.githooks.CopyGitHooksTask
 import io.github.ackeecz.gradle.util.assembleTask
 import io.github.ackeecz.gradle.util.getApplicationAndroidComponents
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import java.io.File
 
 class VerificationsPlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
         with(project) {
-            setUpDetekt()
             setUpCodeCoverage()
             setUpLint()
             setUpCopyGitHooks()
         }
-    }
-
-    private fun Project.setUpDetekt() {
-        val configFile = File(rootDir, "detekt-config-common.yml")
-        FetchDetektConfigTask.registerTask(this, DETEKT_CONFIG_URL, configFile)
     }
 
     private fun Project.setUpCodeCoverage() {
@@ -49,10 +41,5 @@ class VerificationsPlugin : Plugin<Project> {
         project.getApplicationAndroidComponents().onVariants { variant ->
             variant.assembleTask(project) { it.dependsOn(copyGitHooksProvider) }
         }
-    }
-
-    companion object {
-
-        private const val DETEKT_CONFIG_URL = "https://raw.githubusercontent.com/AckeeCZ/styleguide/master/android/detekt-config.yml"
     }
 }
